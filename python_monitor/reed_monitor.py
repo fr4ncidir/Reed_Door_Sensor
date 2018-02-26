d6a5502d138c48017d4698f4cd9812be11a9f901
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2018-02-26 13:01:25 +0100

    Add syslog support

--- python_monitor/reed_monitor.py
+++ python_monitor/reed_monitor.py
@@ -25,80 +25,105 @@
 import sys,serial,logging
 import smtplib
 from datetime import datetime
-from email.message import EmailMessage
+from logging import handlers,Formatter
 
 logging.basicConfig(format="%(levelname)s %(asctime)-15s %(message)s",level=logging.DEBUG)
+
+monitorLog = logging.getLogger("monitorLog")
+handle_monitorLog = logging.handlers.SysLogHandler(address="/dev/log")
+handle_monitorLog.setFormatter(logging.Formatter())
+monitorLog.addHandler(handle_monitorLog)
+
+plainLog = logging.getLogger("plainLog")
 identifier = "123456789"
 
 TS_format = "{:%Y-%m-%d %H:%M:%S.%f}"
 
+e_msg = {
+    "From":"",
+    "To":""
+    }
+
 def handle_open_door(timestamp):
-	msg = EmailMessage()
-	msg.set_content("""
+    e_msg["email"] = """
 {} - Door opened at {} -
 
 auto-generated mail: do not reply
-	""".format(identifier,timestamp))
-	msg["Subject"] = "Door monitoring system - Door opening"
-	msg["From"] = "origin@gmail.com"
-	msg["To"] = "sgsi-alarms@lists.infn.it"
-	s = smtplib.SMTP("postino.cnaf.infn.it")
-	s.send_message(msg)
-	s.quit()
-	
+""".format(identifier,timestamp)
+    e_msg["Subject"] = "Door monitoring system - Door opening"
+    plainLog.warning(e_msg["email"])
+    monitorLog.warning(e_msg["email"])
+    #msg["From"] = "origin@gmail.com"
+    #msg["To"] = "sgsi-alarms@lists.infn.it"
+    #s = smtplib.SMTP("postino.cnaf.infn.it")
+    #s.send_message(msg)
+    #s.quit()
+
 def handle_closed_door(timestamp):
-	msg = EmailMessage()
-	msg.set_content("""
+    e_msg["email"] = """
 {} - Door closed at {} -
 
 auto-generated mail: do not reply
-	""".format(identifier,timestamp))
-	msg["Subject"] = "Door monitoring system - Door closing"
-	msg["From"] = "origin@gmail.com"
-	msg["To"] = "destination@gmail.com"
-	s = smtplib.SMTP("smtp.server.address.com")
-	s.send_message(msg)
-	s.quit()
+    """.format(identifier,timestamp)
+    e_msg["Subject"] = "Door monitoring system - Door closing"
+    plainLog.warning(e_msg["email"])
+    monitorLog.warning(e_msg["email"])
+    #msg["From"] = "origin@gmail.com"
+    #msg["To"] = "destination@gmail.com"
+    #s = smtplib.SMTP("smtp.server.address.com")
+    #s.send_message(msg)
+    #s.quit()
 	
 def handle_system_failure(timestamp,error_str):
-	msg = EmailMessage()
-	msg.set_content("""
+    e_msg["email"] = """
 {} - Door monitoring system failure at {} -
 {}
 
 auto-generated mail: do not reply
-	""".format(identifier,timestamp,error_str))
-	msg["Subject"] = "Door monitoring system failure"
-	msg["From"] = "origin@gmail.com"
-	msg["To"] = "destination@gmail.com"
-	s = smtplib.SMTP("smtp.server.address.com")
-	s.send_message(msg)
-	s.quit()
+    """.format(identifier,timestamp,error_str)
+    e_msg["Subject"] = "Door monitoring system failure"
+    plainLog.warning(e_msg["email"])
+    monitorLog.warning(e_msg["email"])
+    #msg["From"] = "origin@gmail.com"
+    #msg["To"] = "destination@gmail.com"
+    #s = smtplib.SMTP("smtp.server.address.com")
+    #s.send_message(msg)
+    #s.quit()
 
 def main(args):
-	try:
-		ser = serial.Serial(port="COM13",baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=4)
-		while True:
-			line = ser.read(4).decode("cp1252")
-			if len(line)<4:
-				print("Timeout Reached")
-			else:
-				str_timestamp = TS_format.format(datetime.now())
-				if line=="-CO-":
-					logging.warning("{} --- THE DOOR WAS OPENED".format(str_timestamp))
-					handle_open_door(str_timestamp)
-				elif line=="+OC+":
-					logging.warning("{} --- THE DOOR WAS CLOSED".format(str_timestamp))
-					handle_closed_door(str_timestamp)
-				elif line=="*UP*":
-					logging.info("Device is up and running")
-				else:
-					raise serial.SerialException("Unknown packet {}".format(line))
-	except Exception:
-		error_str = "Exception: {}".format(sys.exc_info()[1])
-		logging.error(error_str)
-		handle_system_failure(TS_format.format(datetime.utcnow()),error_str)
-	return 0
+    try:
+        ser = serial.Serial(port="/dev/ttyUSB0",baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=4)
+        run_flag = True
+        while True:
+            line = ser.read(4).decode("cp1252")
+            if len(line)<4:
+                plainLog.warning("Timeout Reached")
+            else:
+                str_timestamp = TS_format.format(datetime.now())
+                if line=="-CO-":
+                    plainLog.warning("{} --- THE DOOR WAS OPENED".format(str_timestamp))
+                    monitorLog.warning("{} --- THE DOOR WAS OPENED".format(str_timestamp))
+                    handle_open_door(str_timestamp)
+                    run_flag = True
+                elif line=="+OC+":
+                    plainLog.warning("{} --- THE DOOR WAS CLOSED".format(str_timestamp))
+                    monitorLog.warning("{} --- THE DOOR WAS CLOSED".format(str_timestamp))
+                    handle_closed_door(str_timestamp)
+                    run_flag = True
+                elif line=="*UP*":
+                    plainLog.info("Device is up and running")
+                    if run_flag:
+                        monitorLog.info("Device is up and running")
+                        run_flag = False
+                else:
+                    run_flag = True
+                    raise serial.SerialException("Unknown packet {}".format(line))
+    except Exception:
+        error_str = "Exception: {}".format(sys.exc_info()[1])
+        plainLog.error(error_str)
+        monitorLog.error(error_str)
+        handle_system_failure(TS_format.format(datetime.utcnow()),error_str)
+    return 0
 
 if __name__ == '__main__':
-	sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))

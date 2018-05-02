cb48050df5d2621c2e2b82b85d2c317b479ce5ee
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2018-05-02 18:00:02 +0200

    Add some comments

--- python_monitor/reed_monitor.py
+++ python_monitor/reed_monitor.py
@@ -27,6 +27,7 @@ import logging
 import serial
 from datetime import datetime
 
+# Setup main logging format and timestamp format
 logging.basicConfig(format="%(levelname)s %(asctime)-15s %(message)s",
                     level=logging.DEBUG)
 doorLog = logging.getLogger("DoorLog")
@@ -40,12 +41,14 @@ def handle_door_event(notification):
 
 
 def main_service(serial_port, device_id):
+    # Method called by systemd start of service
     import sdnotify
     from logging import handlers, Formatter
 
     n = sdnotify.SystemdNotifier()
     n.notify("READY=1")
 
+    # log is redirected to syslog
     handle_doorLog = logging.handlers.SysLogHandler(address="/dev/log")
     handle_doorLog.setFormatter(logging.Formatter())
     doorLog.addHandler(handle_doorLog)
@@ -54,17 +57,20 @@ def main_service(serial_port, device_id):
     last_time_up = None
     while reopen:
         try:
+            # opening serial port
             ser = serial.Serial(port=serial_port, baudrate=9600,
                                 parity=serial.PARITY_NONE,
                                 stopbits=serial.STOPBITS_ONE,
                                 bytesize=serial.EIGHTBITS, timeout=4)
         except:
+            # exiting in case of exception
             doorLog.critical("id={} - exception={}".format(device_id,
                              sys.exc_info()[1]))
             n.notify("STATUS=exception while opening serial port")
             n.notify("STOPPING=1")
             return 1
 
+        # reopen flag in case of reading mistakes
         reopen = False
         n.notify("STATUS=successfully opened serial port")
         previous = "S"
@@ -73,16 +79,19 @@ def main_service(serial_port, device_id):
             timestamp = TS_format.format(datetime.now())
 
             try:
-                character = ser.read(1).decode("utf-8")
+                # read a char from serial
+                character = ser.read(1)
             except:
                 doorLog.critical("id={} - exception={}"
                                  .format(device_id, sys.exc_info()[1]))
                 exception_counter += 1
                 if exception_counter <= 5:
+                    # retry to read 5 times
                     n.notify("STATUS={} #{}".format(sys.exc_info()[1],
                                                     exception_counter))
                     continue
                 else:
+                    # if after 5 attempts read is impossible, exits
                     n.notify("STATUS=exception while reading serial port")
                     n.notify("STOPPING=1")
                     ser.close()
@@ -113,13 +122,20 @@ def main_service(serial_port, device_id):
                         doorLog.warning("Read unknown '{}'(len={}) from serial port".format(character.encode('hex'),
                                         len(character)))
                     else:
+                        # if we have two mistakes in reading, try close
+                        # and reopen the serial port
                         reopen = True
                         break
                     character = "E"
                 previous = character
+        # when closing and reopening because of mistakes, we check that
+        # not too much time has passed. In case, we exit and call
+        # the event handler
         last_time_up_delta_seconds = (datetime.now()-last_time_up).total_seconds()
         if ((last_time_up is not None) and (last_time_up_delta_seconds > 180)):
-            doorLog.critical("Timeout in Up-And-Running communication reached: {} seconds".format(last_time_up_delta_seconds))
+            notification = "Timeout in Up-And-Running communication reached: {} seconds".format(last_time_up_delta_seconds)
+            handle_door_event(notification)
+            doorLog.critical(notification)
             n.notify("STOPPING=1")
             ser.close()
             return 1
@@ -135,15 +151,18 @@ def main_on_request(serial_port, device_id):
     last_time_up = None
     while reopen:
         try:
+            # opening serial port
             ser = serial.Serial(port=serial_port, baudrate=9600,
                                 parity=serial.PARITY_NONE,
                                 stopbits=serial.STOPBITS_ONE,
                                 bytesize=serial.EIGHTBITS, timeout=4)
         except:
+            # exiting in case of exception
             doorLog.critical("id={} - exception={}".format(device_id,
                              sys.exc_info()[1]))
             return 1
 
+        # reopen flag in case of reading mistakes
         reopen = False
         previous = "S"
         while True:
@@ -152,10 +171,13 @@ def main_on_request(serial_port, device_id):
             try:
                 character = ser.read(size=1)
             except KeyboardInterrupt:
+                # in the on-request case, Ctrl-C is to be considered
+                # as a legitimate stop request
                 doorLog.info("Keyboard interrupt!")
                 ser.close()
                 return 0
             except:
+                # exits immediately if reading is impossible
                 doorLog.critical("id={} - exception={}".format(device_id,
                                  sys.exc_info()[1]))
                 ser.close()
@@ -195,6 +217,7 @@ def main_on_request(serial_port, device_id):
 if __name__ == '__main__':
     import argparse
     import subprocess
+    # parsing of command line arguments
     parser = argparse.ArgumentParser(description="Debian reed door sensor monitor")
     parser.add_argument("run-mode", choices=["service", "on-request"],
                         help="identifies if the script runs as standalone or systemd service")
@@ -203,6 +226,7 @@ if __name__ == '__main__':
                         help="address in /dev of the device port")
     args = vars(parser.parse_args())
 
+    # retrieving constructor serial id of device
     bashCommand = "udevadm info -q all -n {}".format(args["sp"])
     process1 = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     process2 = subprocess.Popen(["grep", "ID_SERIAL_SHORT=*"],
@@ -213,6 +237,7 @@ if __name__ == '__main__':
     output, error = process2.communicate()
     device_id = output.split("=")[1]
 
+    # calling main procedure
     if args["run-mode"] == "service":
         sys.exit(main_service(args["sp"], device_id))
     else:

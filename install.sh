80abfbafdbd059bfbe9c7038ff1726f4d8e940bf
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2018-10-06 11:02:59 +0200

    delete useless file, add udev exampl

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #! /bin/bash
 
-if [ "$EUID" -ne 0 ] 
-    then 
+# exits if command returns nonzero
+set -e
+
+# this script needs high privileges to write two files in 
+# systemd and udev folders, install and remove packages
+if [ "$EUID" -ne 0 ] ; then
     echo Please run as root
     exit
 fi
 
 WD=`pwd`
-if [ -b "$1" ]
-    then
+if [ -b "$1" ] ; then
     echo "Serial port <$1> was found"
 else
     echo "Serial port <$1> not found"
@@ -16,13 +19,12 @@ else
 fi
 
 echo Installing packet dependencies...
-yum install git pyserial
+yum install pyserial
 
 echo Removing ModemManager...
 yum remove ModemManager
 
-echo Cloning repositories...
-git clone https://github.com/fr4ncidir/Reed_Door_Sensor.git
+echo Cloning sdnotify repository...
 git clone https://github.com/bb4242/sdnotify.git
 
 cd ./sdnotify
@@ -36,10 +38,8 @@ sed -i "s@__PYTHON__PATH@`which python`@g" temp.service
 sed "s@__FULL_SCRIPT_PATH__@`realpath reed_monitor.py`@g" temp.service > /etc/systemd/system/reed_daemon.service
 
 echo Creating udev rule...
-#SNUMBER=`udevadm info -q all -n $1 | grep `
-cd /etc/udev/rules.d
-if [ -e 10-local.rules ]
-    then
+#SNUMBER=`udevadm info -q all -n $1 | grep -o "regex"`
+if [ -e /etc/udev/rules.d/10-local.rules ] ; then
     echo Rule name exists, aborting
     exit
 fi

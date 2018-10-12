2cf8d8a6dd44d887653d57acac8b343746c6bd9b
Francesco Antoniazzi <francesco.antoniazzi1991@gmail.com>
2018-10-12 11:01:11 +0200

    Better README

--- README.md
+++ README.md
@@ -1,10 +1,32 @@
-## ModemManager issue
+## Hardware
+ESP8266 Huzzah, programmed by Arduino IDE
+ST Nucleo32, programmed by [mbed](https://www.mbed.com/en/) online compiler.
+The software is available in the `./reed_door_sensor/reed_door_sensor.ino` file.
 
-Read the issue opened [here](https://bugs.launchpad.net/modemmanager/+bug/700261).
+## Install
+Requirements: `git`
 
-ModemManager occupies the serial port *before* the reed_door.service. This creates some issues when later opening the port properly.
-
+To install the services and all the needed software, *_as root_*, call the following:
 ```
-yum remove ModemManager
+$ git clone "this repository"
+$ cd "this repository folder"
+$ bash install.sh /dev/ttyXYZ
 ```
-is not always possible.
+
+Will install:
+#### a) pyserial
+Python package for serial communication
+#### b) sdnotify
+Python package available on github, to setup systemd services.
+#### c) ModemManager
+Read the issue opened [here](https://bugs.launchpad.net/modemmanager/+bug/700261). ModemManager occupies the serial port *before* the reed_door.service. This creates some issues when later opening the port properly. It should be removed.
+#### udev rule
+#### systemd service 
+calling
+```
+python ./python_monitor/reed_monitor.py service --sp /dev/ttyUSB0
+```
+python script.
+
+## Working
+Plug the Huzzah, or the STM Nucleo32, then the service should run and start to log events in `/var/log/messages`.
--- reed_door_sensor/reed_door_sensor.ino
+++ reed_door_sensor/reed_door_sensor.ino
@@ -18,14 +18,29 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  * 
- * 
  */
 
+// Comment the board you are not using.
 #define NUCLEO_32
 //#define HUZZAH
 
-#ifdef HUZZAH
+//------------------------------------
+// Hyperterminal configuration
+// 9600 bauds, 8-bit data, no parity
+//------------------------------------
 
+/*
+ * The communication protocol of this system is the following:
+ * Every second, the Nucleo board sends
+ *   U
+ * to certify it's alive.
+ * When the sensor switches from closed to open, it sends
+ *   O
+ * Otherwise, switching from open to closed
+ *   C
+ */
+
+#ifdef HUZZAH
 #define SENSOR_IN   5
 #define SENSOR_OUT  4
 /*
@@ -65,55 +80,37 @@ void loop() {
   }
   mod4++;
 }
-
 #endif
 
-#ifdef NUCLEO_32
 
+#ifdef NUCLEO_32
 #include "mbed.h"
 
-//------------------------------------
-// Hyperterminal configuration
-// 9600 bauds, 8-bit data, no parity
-//------------------------------------
-
 Serial pc(USBTX, USBRX, 9600);
-
 DigitalIn sensor_in(PB_6,PullDown);
 DigitalOut sensor_out(PB_7,1);
 DigitalOut myled(LED1);
 
 int main() {
-	int old_s,new_s;
-	uint8_t mod4=0;
-	old_s = sensor_in.read();
-	pc.baud(9600);
-	pc.format(8,SerialBase::None,1);
-	while(1) {
-		wait(0.25);
-		new_s = sensor_in.read();
-		if (new_s!=old_s) {
-			if (old_s==1) pc.putc("O");
-			else pc.putc("C");
-			old_s = new_s;
-		}
-		myled = !myled;
-		if (mod4%4==0) {
-			pc.putc("U");
-			mod4 = 0;
-		}
-		mod4++;
-	}
+    int old_s,new_s;
+    uint8_t mod4=0;
+    old_s = sensor_in.read();
+    pc.baud(9600);
+    pc.format(8,SerialBase::None,1);
+    while (1) {
+        wait(0.25);
+        new_s = sensor_in.read();
+        if (new_s!=old_s) {
+            if (old_s==1) pc.putc('O');
+            else pc.putc('C');
+            old_s = new_s;
+        }
+        myled = !myled;
+        if (mod4%4==0) {
+            pc.putc('U');
+            mod4 = 0;
+        }
+        mod4++;
+    }
 }
-
-/*
-The communication protocol of this system is the following:
-Every second, the Nucleo board sends
-	U
-to certify it's alive.
-When the sensor switches from closed to open, it sends
-	O
-Otherwise, switching from open to closed
-	C
-*/
 #endif
